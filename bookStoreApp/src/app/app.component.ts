2df2926fef8b47d9d571661d134a0a696b236489
wnukedev <wnukedev@gmail.com>
2023-08-11 17:42:53 -0500

    Updated code

--- bookStoreApp/src/app/app.component.ts
+++ bookStoreApp/src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  //templateUrl: './app.componentSideNav.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  email: string = "sample@mail.com";
   title = 'bookStoreApp';
+  showFiller = false;
 }
--- bookStoreApp/src/app/app.module.ts
+++ bookStoreApp/src/app/app.module.ts
@@ -4,22 +4,41 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { SidenavComponent } from './sidenav/sidenav.component';
+
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { CounterService } from './shared/services/counter.service';
+import { PublicComponentsModule } from './public-components/public-components.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, SidenavComponent
   ],
   imports: [
     BrowserModule,
-    // AuthModule,
-    // UserModule,
-    // PublicModule,
     SharedModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+
+    PublicComponentsModule
+
   ],
-  providers: [],
+  providers: [
+    CounterService,
+    { provide: 'token', useValue: 'xjkhkdhfkksxjjkskjdfjsjslajdfljlasjdf' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
